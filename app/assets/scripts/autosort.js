020a2041004efd8b13de21519e3cd321c6df8fe3
Roi <68617740+Roizor@users.noreply.github.com>
2023-02-22 19:35:32 -0600

    eslint likes me now

--- app/assets/scripts/autosort.js
+++ app/assets/scripts/autosort.js
@@ -1,17 +1,19 @@
 // Page sorter! No more manual pages!
-let Pages = {};
-let countOnEachPage = 8
-let pagesAmount = Sounds.length/countOnEachPage;
+const Pages = {}
+const countOnEachPage = 8
+// eslint-disable-next-line no-undef
+const pagesAmount = Sounds.length / countOnEachPage
 for (let i = 0; i < pagesAmount; i++) {
-    Pages[i] = [];
+  Pages[i] = []
 }
-let pageCounter = 0;
-let index = 0;
+let pageCounter = 0
+let index = 0
+// eslint-disable-next-line no-undef
 Sounds.forEach(sound => {
-    Pages[pageCounter].push(sound)
-    if(index == countOnEachPage) {
-        pageCounter++;
-        index = 0;
-    }
-    index++;
-});
+  Pages[pageCounter].push(sound)
+  if (index === countOnEachPage) {
+    pageCounter++
+    index = 0
+  }
+  index++
+})
--- app/assets/scripts/loader.js
+++ app/assets/scripts/loader.js
@@ -1,102 +1,102 @@
-var socket = io();
-var keys = document.getElementById('keys');
-var keyList = [];
-var loaded = false;
-var loggedin = false;
-var currentPage = 0;
+const socket = io()
+const keys = document.getElementById('keys')
+let keyList = []
+let loaded = false
+let currentPage = 0
 
 addToHTMLlog('Waiting for server...')
 
 socket.on('server_connected', function () {
-    // _sdsession is session id
-    addToHTMLlog("Connected! Checking for login status..")
-    if (localStorage.getItem('_sdsession')) {
-        socket.emit('Authenticated', localStorage.getItem('_sdsession'))
-        addToHTMLlog('Success! You\'re logged in.')
-        loaded = true;
-
-    } else {
-        addToHTMLlog('Not logged in, requesting login')
-        loaded = true;
-        localStorage.setItem('_sdsid', Math.random().toString().substring(2, 5))
-        socket.emit('c2sr_login', localStorage.getItem("_sdsid"))
-    }
+  // _sdsession is session id
+  addToHTMLlog('Connected! Checking for login status..')
+  if (localStorage.getItem('_sdsession')) {
+    socket.emit('Authenticated', localStorage.getItem('_sdsession'))
+    addToHTMLlog('Success! You\'re logged in.')
+    loaded = true
+  } else {
+    addToHTMLlog('Not logged in, requesting login')
+    loaded = true
+    localStorage.setItem('_sdsid', Math.random().toString().substring(2, 5))
+    socket.emit('c2sr_login', localStorage.getItem('_sdsid'))
+  }
 })
 
 socket.on('s2ca_login', function (s, c) {
-    addToHTMLlog('Request received by server, let\'s log in.')
-    window.location.href = s;
-    localStorage.setItem('_sdl', c)
+  addToHTMLlog('Request received by server, let\'s log in.')
+  window.location.href = s
+  localStorage.setItem('_sdl', c)
 })
 
 socket.on('greenlight', function () {
-    document.getElementById('loading').style.display = "none"
-    loadPage(0)
+  document.getElementById('loading').style.display = 'none'
+  loadPage(0)
 })
 
 socket.on('banish', function () {
-    localStorage.setItem('_sdsession', '');
-    window.location.reload()
+  localStorage.setItem('_sdsession', '')
+  window.location.reload()
 })
 
 setInterval(function () {
-    // Auto refresh, you shouldn't be waiting to connect for longer than 500ms.
-    if (loaded) return;
-    addToHTMLlog('Connection attempt timed out. Retrying...')
-    window.location.reload();
+  // Auto refresh, you shouldn't be waiting to connect for longer than 500ms.
+  if (loaded) return
+  addToHTMLlog('Connection attempt timed out. Retrying...')
+  window.location.reload()
 }, 500)
 
 function addToHTMLlog(text) {
-    document.getElementById('console').innerText += text + '\n';
+  document.getElementById('console').innerText += text + '\n'
 }
 function loadPage(pageNumber) {
-    currentPage = pageNumber;
-    localStorage.setItem('_sdpage', currentPage);
-    keyList = [];
-    const myNode = document.getElementById("keys");
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.lastChild);
-    }
+  currentPage = pageNumber
+  localStorage.setItem('_sdpage', currentPage)
+  keyList = []
+  const myNode = document.getElementById('keys')
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild)
+  }
 
-    Pages[pageNumber].forEach(sound => {
-        keyList.push(sound);
-        let btn = document.createElement('button');
-        btn.className = "keypress btxt";
-        if (sound.key) {
-            btn.setAttribute('data-key', sound.key);
-        } else {
-            let txt = ''
-            sound.keys.forEach(key => {
-                txt += `{${key}}`
-            })
-            btn.setAttribute('data-key', txt);
-        }
-        if (sound.icon) {
-            btn.style.backgroundImage = "url('assets/icons/" + sound.icon + "')"
-        }
-        btn.innerText = sound.name;
-        keys.appendChild(btn)
-    })
-
-    let sbtn = document.createElement('button');
-    sbtn.className = "keypress btxt";
-    sbtn.setAttribute('data-key', 'f19');
-    sbtn.innerText = "Stop All";
-    let reloadbtn = document.createElement('button');
-    reloadbtn.onclick = () => { window.location.reload() };
-    reloadbtn.className = "btxt";
-    reloadbtn.innerText = "Reload";
-    let susdeck = document.createElement('a');
-    susdeck.className = "button"
-    susdeck.style.backgroundImage = "url('assets/icons/susdeck.png')"
-    keys.appendChild(reloadbtn);
-    keys.appendChild(sbtn);
-    keys.appendChild(susdeck);
-    const allKeypress = document.getElementsByClassName('keypress');
-    for (let i = 0; i < allKeypress.length; i++) {
-        allKeypress[i].onclick = (ev) => {
-            if (SoundOnPress) new Audio('press.mp3').play();
-            socket.emit('keypress', allKeypress[i].getAttribute('data-key'))
-        }
+  // eslint-disable-next-line no-undef
+  Pages[pageNumber].forEach(sound => {
+    keyList.push(sound)
+    const btn = document.createElement('button')
+    btn.className = 'keypress btxt'
+    if (sound.key) {
+      btn.setAttribute('data-key', sound.key)
+    } else {
+      let txt = ''
+      sound.keys.forEach(key => {
+        txt += `{${key}}`
+      })
+      btn.setAttribute('data-key', txt)
     }
+    if (sound.icon) {
+      btn.style.backgroundImage = "url('assets/icons/" + sound.icon + "')"
+    }
+    btn.innerText = sound.name
+    keys.appendChild(btn)
+  })
+
+  const sbtn = document.createElement('button')
+  sbtn.className = 'keypress btxt'
+  sbtn.setAttribute('data-key', 'f19')
+  sbtn.innerText = 'Stop All'
+  const reloadbtn = document.createElement('button')
+  reloadbtn.onclick = () => { window.location.reload() }
+  reloadbtn.className = 'btxt'
+  reloadbtn.innerText = 'Reload'
+  const susdeck = document.createElement('a')
+  susdeck.className = 'button'
+  susdeck.style.backgroundImage = "url('assets/icons/susdeck.png')"
+  keys.appendChild(reloadbtn)
+  keys.appendChild(sbtn)
+  keys.appendChild(susdeck)
+  const allKeypress = document.getElementsByClassName('keypress')
+  for (let i = 0; i < allKeypress.length; i++) {
+    allKeypress[i].onclick = (ev) => {
+      // eslint-disable-next-line no-undef
+      if (SoundOnPress) new Audio('press.mp3').play()
+      socket.emit('keypress', allKeypress[i].getAttribute('data-key'))
+    }
+  }
 }
